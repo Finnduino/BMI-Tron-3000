f2aca9d1ec7a35e8b56537eeb3521c813a3c86e6
Daniil Parniukov <dparniukov@gmail.com>
2024-11-09 11:47:09 +0300

    added a function to read a pdf file and transform it into an opencv image

---
+++ image-processing.py
@@ -0,0 +1,27 @@
+from pdf2image import convert_from_path
+from PIL import Image
+import cv2 as cv
+import numpy as np
+import logging
+logger = logging.getLogger(__name__)
+
+def init():
+    Image.MAX_IMAGE_PIXELS = None # so that the pdf files can be read
+    logging.basicConfig(level=logging.INFO)
+
+
+def floor_plan_to_opencv_image(path: str):
+    pdf_pages = convert_from_path(path, 500)
+    if len(pdf_pages) != 1:
+        logger.warning("More than 1 page found, ignoring everything but the first page")
+    return np.array(pdf_pages[0])[:, :, ::-1]
+
+def main():
+    init()
+    opencv_image = floor_plan_to_opencv_image("floor_1.pdf")
+    logger.info("started writing")
+    cv.imwrite("floor_1.png", opencv_image)
+    logger.info("finished writing")
+
+if __name__ == '__main__':
+    main()
